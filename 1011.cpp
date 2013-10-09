66b85ba7986da582a82fd82934c0b981097662ae
Tao J <tao-j@outlook.com>
2013-10-09 14:14:03 +0800

    1011B ACed

--- 1011.cpp
+++ 1011.cpp
@@ -6,8 +6,16 @@
 using namespace std;
 
 int main() {
-
+	long a, b, c;
+	int t;
+	cin >> t;
+	int i = t;
+	while (i--) {
+		cin >> a >> b >> c;
+		if (a + b > c)
+			cout << "Case #" << t - i << ": true\n";
+		else
+			cout << "Case #" << t - i << ": false\n";
+	}
 	return 0;
 }
-
-
